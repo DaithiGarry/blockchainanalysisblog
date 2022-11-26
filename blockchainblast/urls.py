83a93259ae746356c7fb76c8b1789f1a60d056ca
David Garry <daithigarry@hotmail.com>
2022-11-26 18:42:22 +0000

    updated bcbblog view.py file

--- blockchainblast/urls.py
+++ blockchainblast/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("bcbblog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('', include(bcbblog.urls), name='bcbblog_urls'),
 ]
